bfa4c9412644b4d6e765688a2ee08d8da324e816
Ankur Vatsa <ankur.vatsa@gmail.com>
2020-02-22 19:21:45 +0530

    Quick sample of how Connexion does not work for multipart/form-data, multiple file uploading. Will be now modified to single file and then using that somehow, while I keep at finding more and may be even contribute to the fix.

---
+++ app.py
@@ -0,0 +1,5 @@
+from config import app
+
+if __name__ == '__main__':
+    app.run(port=8080)
+
---
+++ controller.py
@@ -0,0 +1,33 @@
+import os
+from app import app
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message': 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
