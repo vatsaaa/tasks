afcb35e03750cb702e713f54837275adaf88aa37
Ankur Vatsa <ankur.vatsa@gmail.com>
2021-11-02 14:45:49 +0530

    [Issue-3]: Setup remote redis database and write some to acess it

--- app.py
+++ app.py
@@ -2,6 +2,13 @@
 from waitress import serve
 from config.config import app
 
+import redis
+
+def db_connect():
+    r = redis.Redis(host='redis-16582.c264.ap-south-1-1.ec2.cloud.redislabs.com', port=16582, password='vatsaaa@R3D!5')
+    r.set('hello', 'world')
+    print(r.get('hello'))
+
 if __name__ == '__main__':
     serve(app=app, host='127.0.0.1', port=5454)
 
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,14 +3,22 @@ from config.config import app, ALLOWED_EXTENSIONS
 from flask import request, jsonify
 from werkzeug.utils import secure_filename
 from datetime import datetime as dt
+import redis
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+## Just some code to check connectivity to Redis Cloud Instance
+def db_connect():
+    r = redis.Redis(host='redis-16582.c264.ap-south-1-1.ec2.cloud.redislabs.com', port=16582, password='50m3p@55w0rd')
+    r.set('hello', 'world')
+    print(r.get('hello'))
 
 def ping(suffix=None):
     resp_str = "User ping, tasks pong / " + dt.now().strftime("%Y-%m-%d, %H:%M:%S")
     resp = resp_str if suffix is None else resp_str + " / " + suffix
+
+    db_connect()
     
     return resp
